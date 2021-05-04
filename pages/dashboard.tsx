9da1ad54b4f4d827dd407282331a26a6405d6bd0
victorb132 <victor96novais@gmail.com>
2021-05-04 08:53:04 -0300

    Autenticacao com SSR

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,10 @@
+import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react"
 import { AuthContext, singOut } from "../Contexts/AuthContext"
-import { api } from "../services/api";
+import { setupAPIClient } from "../services/api";
+import { api } from "../services/apiClient";
+import { AuthTokenError } from "../services/errors/AuthTokenError";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
@@ -15,3 +19,12 @@ export default function Dashboard() {
     <h1>Dashboard: { user?.email }</h1>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (context) => {
+  const apiClient = setupAPIClient(context);
+  const response = await apiClient.get('/me');
+
+  return {
+    props: {}
+  }
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
+import { GetServerSideProps } from 'next';
 import { FormEvent, useContext, useState } from 'react';
+import { parseCookies } from 'nookies';
+
 import { AuthContext } from '../Contexts/AuthContext';
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -26,3 +30,9 @@ export default function Home() {
     </form>
   )
 }
+
+export const getServerSideProps = withSSRGuest(async (context) => {
+  return {
+    props: {}
+  }
+});
